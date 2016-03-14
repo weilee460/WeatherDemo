72ae8097c8729d4426179fb2b7c295d144fcc1c0
Eagle <wei.lee460@gmail.com>
2016-03-14 14:50:34 +0800

    Add corelocation manager to project, and acquire location info. 2016-03-14

--- WeatherDemo/ViewController.swift
+++ WeatherDemo/ViewController.swift
@@ -7,17 +7,45 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        locationManager.delegate = self
+        
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        if (ios9()) {
+           locationManager.requestAlwaysAuthorization()
+        }
+        locationManager.startUpdatingLocation()
+    }
+    
+    func ios9() -> Bool
+    {
+        print(UIDevice.currentDevice().systemVersion)
+        return UIDevice.currentDevice().systemVersion == "9.2"
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        //取出最后一个
+        let location = locations[locations.count - 1]
+        if (location.horizontalAccuracy > 0) {
+            print("维度：", location.coordinate.latitude)
+            print(location.coordinate.longitude)
+            
+            locationManager.stopUpdatingLocation()
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print(error)
     }
 
 
